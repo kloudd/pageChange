c4c3f441fb4b93a291e311ace505bb7e2ec4cf51
SUMIT SINGH KANWAL <sumit.kanwal@gmail.com>
2013-04-09 00:31:07 +0530

    first commit

---
+++ src/com/example/pagechange/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.pagechange;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	Button b;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		b = (Button) findViewById(R.id.bPage1);
+		b.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+		if (v.getId() == R.id.bPage1) {
+
+			Intent intent = new Intent("com.example.pagechange.SECONDPAGE");
+			startActivity(intent);
+		}
+
+	}
+
+}
---
+++ src/com/example/pagechange/SecondPage.java
@@ -0,0 +1,31 @@
+package com.example.pagechange;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SecondPage extends Activity implements OnClickListener {
+	
+	Button b;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.secondpage);
+		b=(Button) findViewById(R.id.bBack);
+		b.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+		if(v.getId()==R.id.bBack)
+			this.finish();
+		
+	}
+
+}
